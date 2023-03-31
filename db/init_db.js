71abff40f00bd54d8e668f2971a56084e2b5c175
andre <andre.jacques.pujol@gmail.com>
2023-03-31 10:05:41 -0500

    user addresses working and tested, get all products displaying tags

--- db/init_db.js
+++ db/init_db.js
@@ -1,55 +1,61 @@
 const client = require("./client");
 
 const {
-	//user exports
-	createUser,
-	deleteUser,
-	updateUser,
-	getUser,
-	getUserByEmail,
-	getUserById,
-	getAllUsers,
-	//product exports
-	createProduct,
-	editProduct,
-	getAllProducts,
-	getProductByTitle,
+  //user exports
+  createUser,
+  deleteUser,
+  updateUser,
+  getUser,
+  getUserByEmail,
+  getUserById,
+  getAllUsers,
+  //product exports
+  createProduct,
+  editProduct,
+  getAllProducts,
+  getProductByTitle,
 
-	getProductById,
-	getProductsByAuthor,
+  getProductById,
+  getProductsByAuthor,
 
-	//tag exports
-	createTags,
-	getAllTags,
-	getTagById,
+  //tag exports
+  createTags,
+  getAllTags,
+  getTagById,
 
-	//reviews exports
-	createReview,
+  //reviews exports
+  createReview,
   editReview,
   getAllReviews,
   getReviewByProductId,
 
-	//product_tags exports
-	addTagsToProduct,
-	createProductTag,
+  //product_tags exports
+  addTagsToProduct,
+  createProductTag,
 
+  //user address imports
+  createUserAddress,
+  editUserAddress,
+  deleteUserAddress,
+  getAddressByUser,
 } = require("./index");
 const {
-	productsToAdd,
-	usersToAdd,
-	reviewsToAdd,
-	tagsToAdd,
-	productTagsToAdd,
+  productsToAdd,
+  usersToAdd,
+  reviewsToAdd,
+  tagsToAdd,
+  productTagsToAdd,
+  userAddresses,
 } = require("./dummyData");
 const reviews = require("./reviews");
 
 const dropTables = async () => {
-	try {
-		// drop tables in correct order
-		client.connect();
-		console.log("Dropping All Tables");
+  try {
+    // drop tables in correct order
+    client.connect();
+    console.log("Dropping All Tables");
 
-		await client.query(`
+    await client.query(`
     DROP TABLE IF EXISTS cart_products;
     DROP TABLE IF EXISTS product_tags;
     DROP TABLE IF EXISTS user_addresses;
@@ -61,18 +67,18 @@ const dropTables = async () => {
     
     
     `);
-		console.log("Finished Dropping Tables!");
-	} catch (error) {
-		console.error("Error Dropping Tables!");
-		throw error;
-	}
+    console.log("Finished Dropping Tables!");
+  } catch (error) {
+    console.error("Error Dropping Tables!");
+    throw error;
+  }
 };
 
 const createTables = async () => {
-	try {
-		console.log("Starting to Build Tables");
+  try {
+    console.log("Starting to Build Tables");
 
-		await client.query(`
+    await client.query(`
     CREATE TABLE users (
       id SERIAL PRIMARY KEY,
       name varchar(255) NOT NULL,
@@ -133,115 +139,133 @@ const createTables = async () => {
     
     `);
 
-		console.log("Finished Creating Tables");
-	} catch (error) {
-		console.error("Error Building Tables");
-		throw error;
-	}
+    console.log("Finished Creating Tables");
+  } catch (error) {
+    console.error("Error Building Tables");
+    throw error;
+  }
 };
 
 // create initial data functions
 
 const createInitialUsers = async () => {
-	console.log("Starting to Create Users");
-	try {
-		const users = await Promise.all(usersToAdd.map(createUser));
-		// console.log(users);
-		console.log("finished creating users");
-		return users;
-	} catch (error) {
-		console.error("error creating users");
-		throw error;
-	}
+  console.log("Starting to Create Users");
+  try {
+    const users = await Promise.all(usersToAdd.map(createUser));
+    // console.log(users);
+    console.log("finished creating users");
+    return users;
+  } catch (error) {
+    console.error("error creating users");
+    throw error;
+  }
 };
 
 const createInitialTags = async () => {
-	try {
-		const tags = await Promise.all(tagsToAdd.map(createTags));
-		// console.log(tags);
-		console.log("finished creating tags!");
-		return tags;
-	} catch (error) {
-		console.log("error creating tags!");
-		throw error;
-	}
+  try {
+    const tags = await Promise.all(tagsToAdd.map(createTags));
+    // console.log(tags);
+    console.log("finished creating tags!");
+    return tags;
+  } catch (error) {
+    console.log("error creating tags!");
+    throw error;
+  }
 };
 
 const createInitialProducts = async () => {
-	console.log("Starting to create products");
-	try {
-		const products = await Promise.all(productsToAdd.map(createProduct));
-		// console.log(products);
-		console.log("finished creating products");
-		return products;
-	} catch (error) {
-		console.error("error creating products");
-		throw error;
-	}
+  console.log("Starting to create products");
+  try {
+    const products = await Promise.all(productsToAdd.map(createProduct));
+    // console.log(products);
+    console.log("finished creating products");
+    return products;
+  } catch (error) {
+    console.error("error creating products");
+    throw error;
+  }
 };
 
 const createInitialReviews = async () => {
-	console.log("Starting to create reviews");
-	try {
-		const reviews = await Promise.all(reviewsToAdd.map(createReview));
-		// console.log(reviews);
-		console.log("finished creating reviews");
-		return reviews;
-	} catch (error) {
-		console.log("error creating reviews");
-		throw error;
-	}
+  console.log("Starting to create reviews");
+  try {
+    const reviews = await Promise.all(reviewsToAdd.map(createReview));
+    // console.log(reviews);
+    console.log("finished creating reviews");
+    return reviews;
+  } catch (error) {
+    console.log("error creating reviews");
+    throw error;
+  }
 };
 
 async function rebuildDB() {
-	try {
-		await dropTables();
-		await createTables();
-		//initial funcs
-		await createInitialUsers();
-		await createInitialTags();
-		await createInitialProducts();
-		
-		await createInitialReviews();
-    await createReview({score: 7, title: "our test review", content: "this review is for testing", reviewerId: 1, productId: 2});
-		//user funcs
-		//await deleteUser(4);
-		await getUser({
-			email: "Nicolerules@mymail.com",
-			password: "ojwasterrible",
-		});
-		await updateUser(4, { email: "test@test.com" });
-		await getUserById(4);
-		await getAllUsers();
-		// //product funcs
-		await editProduct(3, { title: "edited title" });
+  try {
+    await dropTables();
+    await createTables();
+    //initial funcs
+    await createInitialUsers();
+    await createInitialTags();
+    await createInitialProducts();
 
-		console.log(productTagsToAdd, 'productTagsToAdd')
-		const addedProductTags = productTagsToAdd.map((e) => {
-			console.log(e,' (((')
-			addTagsToProduct(e.productId, e.tagId);
-		});
-		await Promise.all(addedProductTags);
-		await getAllProducts();
-		await getProductByTitle("To Kill A Mocking Bird");
+    await createInitialReviews();
+    await createReview({
+      score: 7,
+      title: "our test review",
+      content: "this review is for testing",
+      reviewerId: 1,
+      productId: 2,
+    });
+    //user funcs
+    //await deleteUser(4);
+    await getUser({
+      email: "Nicolerules@mymail.com",
+      password: "ojwasterrible",
+    });
+    await updateUser(4, { email: "test@test.com" });
 
-		await getProductById(1);
-		await getProductsByAuthor("Harper Lee");
+    await getUserById(4);
 
+    const addedUserAddresses = userAddresses.map((address) => {
+      createUserAddress(address);
+    });
+    await Promise.all(addedUserAddresses);
 
-		const tagNumber1 = await getTagById(1);
-		console.log(tagNumber1, 'I am tag number 1!!');
-    
+    console.log(await getAddressByUser(1), "LINE 234");
+    console.log(
+      await editUserAddress(1, { name: "Jimbo" }),
+      "user addred updated"
+    );
+    console.log(await deleteUserAddress(1), " user address deleted");
+
+    console.log(await getAllUsers(), "LINE 236");
+    // //product funcs
+    await editProduct(3, { title: "edited title" });
+
+    console.log(productTagsToAdd, "productTagsToAdd");
+    const addedProductTags = productTagsToAdd.map((e) => {
+      console.log(e, " (((");
+      addTagsToProduct(e.productId, e.tagId);
+    });
+    await Promise.all(addedProductTags);
+    console.log(await getAllProducts(), "LINE 255");
+    await getProductByTitle("To Kill A Mocking Bird");
+
+    await getProductById(1);
+    await getProductsByAuthor("Harper Lee");
+
+    const tagNumber1 = await getTagById(1);
+    console.log(tagNumber1, "I am tag number 1!!");
+
     await getAllReviews();
-    await editReview(1, {title: "WE EDITED THE TITLE"});
+    await editReview(1, { title: "WE EDITED THE TITLE" });
     await getReviewByProductId(2);
-	} catch (error) {
-		console.log("Error during rebuildDB");
-		throw error;
-	}
-
+  } catch (error) {
+    console.log("Error during rebuildDB");
+    throw error;
+  }
 }
 
 rebuildDB()
-	.catch(console.error)
-	.finally(() => client.end());
+  .catch(console.error)
+  .finally(() => client.end());
